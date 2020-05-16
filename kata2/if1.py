8b3f5fbdda973021f9856308976a9fe00900b7c8
Miguel Angel <mimaca359@gmail.com>
2020-05-16 12:39:14 +0200

    kata2

--- kata2/if1.py
+++ kata2/if1.py
@@ -6,6 +6,7 @@ imprima por pantalla si la contraseña introducida por el usuario coincide con l
 password = "contraseña"
 
 password_del_usuario = input("Introduzca la contraseña: ")
+password_del_usuario = password_del_usuario.lower()
 
 if password == password_del_usuario:
     print("El password es correcto")
